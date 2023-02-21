df116b4c59177fb8798858b7b1b5bb1000106f4d
Michael Huang <michaelyhuang23@gmail.com>
2023-02-21 12:50:20 -0500

    EdgeConv coded up

--- tools/gnn_layers.py
+++ tools/gnn_layers.py
@@ -27,7 +27,7 @@ class NodeConv(MessagePassing):
         edge_attr = self.pass_map(edge_attr)
         message_received = self.propagate(edge_index, edge_attr=edge_attr) * deg_inv[..., None]
 
-        out = self.relu(x + message_received)  # ReLU(Wx + sum_neighbor WE)
+        out = self.relu(x + message_received)  # ReLU(xW + sum_neighbor EW)
 
         return out
 
@@ -35,4 +35,29 @@ class NodeConv(MessagePassing):
         # edge_attr has shape [E, out_channels]
         return edge_attr
 
+
+class EdgeConv(MessagePassing):
+    def __init__(self, in_node_channels, in_edge_channels, out_edge_channels):
+        super().__init__(aggr='add')  # "Add" aggregation (Step 5).
+        self.self_map = Linear(in_edge_channels, out_edge_channels)
+        self.pass_map = Linear(in_node_channels*2, out_edge_channels)
+        self.relu = ReLU()
+
+    def reset_parameters(self):
+        self.self_map.reset_parameters()
+        self.pass_map.reset_parameters()
+
+    def forward(self, x, edge_index, edge_attr):
+        # x has shape [N, in_channels]
+        # edge_index has shape [2, E]
+
+        edge_attr = self.self_map(edge_attr)
+
+        row, col = edge_index
+
+        message_received = self.pass_map(torch.concat([x[row], x[col]], dim=-1)) # from the two nodes
+
+        out = self.relu(edge_attr + message_received)  # ReLU(EW + [X,X]W)
+
+        return out
 
