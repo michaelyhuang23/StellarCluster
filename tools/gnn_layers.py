6973eb7834e084481a4890b9eecd8fb3b77632de
Michael Huang <55302647+michaelyhuang23@users.noreply.github.com>
2023-04-11 10:58:43 -0400

    feature encoder

--- tools/gnn_layers.py
+++ tools/gnn_layers.py
@@ -1,3 +1,4 @@
+import numpy as np
 import torch
 from torch.nn import Linear, ReLU, MultiheadAttention
 from torch_geometric.nn import MessagePassing
@@ -62,50 +63,43 @@ class EdgeConv(MessagePassing):
         return out
 
 
-class EdgeAttn(MessagePassing):
-    def __init__(self, in_edge_channels, out_edge_channels, heads):
-        super().__init__(aggr=None) # custom aggregator
-        self.multihead_attn = MultiheadAttention(in_edge_channels, heads)
-        self.relu = ReLU()
-
-    def reset_parameters(self):
-        self.self_map.reset_parameters()
-        self.pass_map.reset_parameters()
-        self.multihead_attn.reset_parameters()
-
-    def forward(self, x, edge_index, edge_attr):
-        # x has shape [N, in_channels]
-        # edge_index has shape [2, E]
-
-        row, col = edge_index
-        print(x.shape)
-        print(row)
-        print(col)
-        message_received = self.propagate(edge_index, size=(x.size(0), x.size(0)), edge_attr=edge_attr)
-        print(message_received.shape)
-        edge_attr = self.self_map(edge_attr)
-
-        
-
-        message_received = self.pass_map(torch.concat([x[row], x[col]], dim=-1)) # from the two nodes
-
-        out = self.relu(edge_attr + message_received)  # ReLU(EW + [X,X]W)
-
-        return out
-
-    def aggregate(self, inputs, index):
-        self.assert_sorted_index(index)
-        print(inputs.shape)
-        print(index)
-        return inputs
 
 
-    def message(self, edge_attr):
-        # edge_attr has shape [E, out_channels]
-        print(edge_attr.shape)
-        return edge_attr
+class FeatureEncoder(nn.Module):
+    def __init__(self, flen=13, d=13, n=10000):
+        self.n = n
+        self.d = d
+        self.flen = flen
+        freq_method = []
+        for i in range(self.d):
+            k = (i//2)*2
+            inv_freq = 1/(self.n ** (k/self.d))
+            if i % 2 == 0:
+                freq_method.append((inv_freq, True))
+            else:
+                freq_method.append((inv_freq, False))
 
-    def assert_sorted_index(self, index: Optional[Tensor]):
-        if index is not None and not torch.all(index[:-1] <= index[1:]):
-            raise ValueError("Can not perform aggregation since the 'index' "
-                             "tensor is not sorted")
+        freq_methods = [np.random.permutation(freq_method) for i in range(self.flen)]
+        self.inv_freqs = torch.tensor([[inv_freq for (inv_freq, method) in freq_method] for freq_method in freq_methods]).float()
+        self.methods = torch.tensor([[method for (inv_freq, method) in freq_method] for freq_method in freq_methods]).bool()
+
+
+    def encode_feature(self, features, sin_inv_freqs, cos_inv_freqs): # feature is n by 13, # sin_inv_ferqs is 13 by d
+        sin_encodings = torch.sum(torch.sin(torch.einsum('ij,jk->ijk', features, sin_inv_freqs)), 1) # collapse j dim
+        cos_encodings = torch.sum(torch.cos(torch.einsum('ij,jk->ijk', features, cos_inv_freqs)), 1) # collapse j dim
+        return None
+
+    def encode_feature_column(self, features, inv_freqs, methods): # feature is n by 13, inv_freqs is 13
+        methods = methods.float()
+        sinusoidal_features = torch.sin(features * inv_freqs[None, ...]) * methods[None, ...] + torch.cos(features * inv_freqs[None, ...]) * (1-methods[None, ...])
+        return torch.sum(sinusoidal_features, dim=-1)
+
+    def forward(self, features):
+        encodings = []
+        for i in range(self.d):
+            column_encoding = self.encode_feature_column(features, self.inv_freqs[:,i], self.methods[:,i])
+            encodings.append(column_encoding)
+        encoded_features = torch.stack(encodings, dim=1) # should be n by self.d
+        return encoded_features
+
+
