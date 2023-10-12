d300aee4536f3f336524936557809af64e36bdc5
Michael Huang <michaelyhuang23@gmail.com>
2023-10-12 14:03:01 -0400

    failed runs

---
+++ shell/run_gaia_test_stitch.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+#SBATCH --gres=gpu:volta:1
+#SBATCH --cpus-per-task=20
+#SBATCH -o gaia_test_stitch_snc_output.txt
+#SBATCH --job-name=snc
+
+pwd
+
+module load anaconda/2023a
+
+cd notebooks/gaia_test
+python gaia_test_stitch_snc.py
+
